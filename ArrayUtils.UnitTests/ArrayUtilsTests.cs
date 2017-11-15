ab4d4a8135849ddcc8e7d498b1c28045b257a7e5
Alexander Yakusik <alexander.yakusik@gmail.com>
2017-11-15 13:44:41 +0300

    Implemented BinarySearch algorithm and created tests for it.

---
+++ ArrayUtils.UnitTests/ArrayUtilsTests.cs
@@ -0,0 +1,106 @@
+﻿namespace ArrayUtils.UnitTests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class ArrayUtilsTests
+    {
+        [Test, TestCaseSource(typeof(ArrayUtilsTestsData), nameof(ArrayUtilsTestsData.BinarySearchUnsortedArrayTestCases))]
+        public void BinarySearch_UnsortedArrayPassed_ArgumentExceptionThrown<T>(T[] array, T soughtElement, IComparer<T> comparer)
+        {
+            Assert.Throws<ArgumentException>(() => ArrayUtils.BinarySearch(array, soughtElement, comparer));
+        }
+
+        [Test, TestCaseSource(typeof(ArrayUtilsTestsData), nameof(ArrayUtilsTestsData.BinarySearchTestCases))]
+        public int BinarySearch_CorrectArrayPassed_SearchesCorrectly<T>(T[] sourceArray, T soughtElement, IComparer<T> comparer)
+        {
+            return ArrayUtils.BinarySearch(sourceArray, soughtElement, comparer);
+        }
+
+        [TestCase(null, 1, null)]
+        public void BinarySearch_NullSourceArrayPassed_ArgumentNullExceptionThrown<T>(T[] sourceArray, T soughtElement, IComparer<T> comparer)
+        {
+            Assert.Throws<ArgumentNullException>(() => ArrayUtils.BinarySearch(sourceArray, soughtElement, comparer));
+        }
+
+        [Test, TestCaseSource(typeof(ArrayUtilsTestsData), nameof(ArrayUtilsTestsData.BinarySearchNullArrayTestCases))]
+        public void BinarySearch_NullComparerPassed_ArgumentNullExceptionThrown<T>(T[] sourceArray, T soughtElement, IComparer<T> comparer)
+        {
+            Assert.Throws<ArgumentNullException>(() => ArrayUtils.BinarySearch(sourceArray, soughtElement, comparer));
+        }
+
+        private class ArrayUtilsTestsData
+        {
+            public static IEnumerable BinarySearchTestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(
+                        new[] { -100, 23, 6, 77, 193440, 29302934 },
+                        -100,
+                        Comparer<int>.Default)
+                        .Returns(0);
+                    yield return new TestCaseData(
+                        new[] { -100, 23, 6, 77, 193440, 29302934 },
+                        29302934,
+                        Comparer<int>.Default)
+                        .Returns(5);
+                    yield return new TestCaseData(
+                        new[] { -100, 23, 6, 77, 193440, 29302934 },
+                        77,
+                        Comparer<int>.Default)
+                        .Returns(3);
+                    yield return new TestCaseData(
+                        new[] { -100, 23, 6, 77, 193440, 29302934 },
+                        5,
+                        Comparer<int>.Default)
+                        .Returns(-1);
+                    yield return new TestCaseData(
+                        new[] { 0, 2, 4, 6, 8, 10, 1500000 },
+                        6,
+                        Comparer<int>.Default)
+                        .Returns(3);
+                    yield return new TestCaseData(
+                        new[] { 0, 2, 4, 6, 8, 10, 1500000 },
+                        1500000,
+                        Comparer<int>.Default)
+                        .Returns(6);
+                    yield return new TestCaseData(
+                        new[] { 0, 2, 4, 6, 8, 10, 1500000 },
+                        1500001,
+                        Comparer<int>.Default)
+                        .Returns(-1);
+                }
+            }
+
+            public static IEnumerable BinarySearchUnsortedArrayTestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(
+                        new[] { 3, 2, 1 },
+                        -2,
+                        Comparer<int>.Default);
+                    yield return new TestCaseData(
+                        new[] { -100, -100, -100, -100, -100, -100, -101 },
+                        -2,
+                        Comparer<int>.Default);
+                }
+            }
+
+            public static IEnumerable BinarySearchNullArrayTestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(
+                        null,
+                        1,
+                        Comparer<int>.Default);
+                }
+            }
+        }
+    }
+}
---
+++ ArrayUtils/ArrayUtils.cs
@@ -0,0 +1,80 @@
+﻿namespace ArrayUtils
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class ArrayUtils
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Searches for <paramref name="soughtElement"/> in the sorted <paramref name="array"/>
+        /// using binary algorithm. 
+        /// </summary>
+        /// <typeparam name="T">Array elements type.</typeparam>
+        /// <param name="array">Source array.</param>
+        /// <param name="soughtElement">Element to be sought.</param>
+        /// <param name="comparer">Object to compare array elements.</param>
+        /// <returns>Index of the sought element in the array. If no such element found, returns -1.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="comparer"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="array"/> is not sorted in ascendant order.</exception>
+        public static int BinarySearch<T>(T[] array, T soughtElement, IComparer<T> comparer)
+        {
+            array = array ?? throw new ArgumentNullException($"{nameof(array)} cannot be null.");
+            comparer = comparer ?? throw new ArgumentNullException($"{nameof(comparer)} cannot be null.");
+
+            CheckSortedArray(array, comparer);
+
+            return BinarySearchAlgorithm(array, soughtElement, comparer);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static int BinarySearchAlgorithm<T>(T[] array, T soughtElement, IComparer<T> comparer)
+        {
+            int leftIndex = 0, rightIndex = array.Length - 1;
+
+            if (leftIndex > rightIndex)
+            {
+                return -1;
+            }
+
+            while (leftIndex < rightIndex)
+            {
+                int currentIndex = (leftIndex + rightIndex) / 2;
+
+                if (comparer.Compare(soughtElement, array[currentIndex]) <= 0)
+                {
+                    rightIndex = currentIndex;
+                }
+                else
+                {
+                    leftIndex = currentIndex + 1;
+                }
+            }
+
+            if (comparer.Compare(array[rightIndex], soughtElement) == 0)
+            {
+                return rightIndex;
+            }
+
+            return -1;
+        }
+
+        private static void CheckSortedArray<T>(T[] array, IComparer<T> comparer)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                if (comparer.Compare(array[i], array[i + 1]) > 0)
+                {
+                    throw new ArgumentException($"{nameof(array)} must be sorted in ascending order.");
+                }
+            }
+        }
+
+        #endregion
+    }
+}
