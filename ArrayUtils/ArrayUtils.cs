12272471fd9b0ccfbf6b787520b539bc71cc1a92
Alexander Yakusik <alexander.yakusik@gmail.com>
2017-11-22 00:25:16 +0300

    Added Comparison<T> version in ArrayUtils.BinarySearch. Also added possibility to search in the part of an array.

--- ArrayUtils/ArrayUtils.cs
+++ ArrayUtils/ArrayUtils.cs
@@ -25,16 +25,89 @@
 
             CheckSortedArray(array, comparer);
 
-            return BinarySearchAlgorithm(array, soughtElement, comparer);
+            return BinarySearchAlgorithm(array, 0, array.Length - 1, soughtElement, comparer);
+        }
+
+        /// <summary>
+        /// Searches for <paramref name="soughtElement"/> in the sorted <paramref name="array"/>
+        /// using binary algorithm. 
+        /// </summary>
+        /// <typeparam name="T">Array elements type.</typeparam>
+        /// <param name="array">Source array.</param>
+        /// <param name="soughtElement">Element to be sought.</param>
+        /// <param name="comparison">Method to compare array elements.</param>
+        /// <returns>Index of the sought element in the array. If no such element found, returns -1.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="array"/> is not sorted in ascendant order.</exception>
+        public static int BinarySearch<T>(T[] array, T soughtElement, Comparison<T> comparison)
+        {
+            if (comparison == null)
+            {
+                return BinarySearch(array, soughtElement, Comparer<T>.Default);
+            }
+
+            return BinarySearch(array, soughtElement, Comparer<T>.Create(comparison));
+        }
+
+        /// <summary>
+        /// Searches for <paramref name="soughtElement"/> in the sorted <paramref name="array"/>
+        /// in range [left; right] using binary algorithm. 
+        /// </summary>
+        /// <typeparam name="T">Array elements type.</typeparam>
+        /// <param name="array">Source array.</param>
+        /// <param name="left">Left index included in search.</param>
+        /// <param name="right">Right index included in search.</param>
+        /// <param name="soughtElement">Element to be sought.</param>
+        /// <param name="comparer">Object to compare array elements.</param>
+        /// <returns>Index of the sought element in the array. If no such element found, returns -1.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="array"/> is not sorted in ascendant order.</exception>
+        public static int BinarySearch<T>(T[] array, int left, int right, T soughtElement, IComparer<T> comparer)
+        {
+            array = array ?? throw new ArgumentNullException($"{nameof(array)} cannot be null.");
+
+            if (left < 0 || right < 0 || left >= array.Length || right >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException($"Indexes are out of range.");
+            }
+            
+            comparer = comparer ?? Comparer<T>.Default;
+
+            CheckSortedArray(array, comparer);
+
+            return BinarySearchAlgorithm(array, left, right, soughtElement, comparer);
+        }
+
+        /// <summary>
+        /// Searches for <paramref name="soughtElement"/> in the sorted <paramref name="array"/>
+        /// in range [left; right] using binary algorithm. 
+        /// </summary>
+        /// <typeparam name="T">Array elements type.</typeparam>
+        /// <param name="array">Source array.</param>
+        /// <param name="left">Left index included in search.</param>
+        /// <param name="right">Right index included in search.</param>
+        /// <param name="soughtElement">Element to be sought.</param>
+        /// <param name="comparison">Method to compare array elements.</param>
+        /// <returns>Index of the sought element in the array. If no such element found, returns -1.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="array"/> is not sorted in ascendant order.</exception>
+        public static int BinarySearch<T>(T[] array, int left, int right, T soughtElement, Comparison<T> comparison)
+        {
+            if (comparison == null)
+            {
+                return BinarySearch(array, left, right, soughtElement, Comparer<T>.Default);
+            }
+
+            return BinarySearch(array, left, right, soughtElement, Comparer<T>.Create(comparison));
         }
 
         #endregion
 
         #region Private methods
 
-        private static int BinarySearchAlgorithm<T>(T[] array, T soughtElement, IComparer<T> comparer)
+        private static int BinarySearchAlgorithm<T>(T[] array, int left, int right, T soughtElement, IComparer<T> comparer)
         {
-            int leftIndex = 0, rightIndex = array.Length - 1;
+            int leftIndex = left, rightIndex = right;
 
             if (leftIndex > rightIndex)
             {
